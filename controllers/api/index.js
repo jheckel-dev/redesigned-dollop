86e6369b8ca4fdc8baf50ef0b71bf7933418ce27
jheckel <jalen.heckel@icloud.com>
2021-12-01 12:35:13 -0800

    updated post routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user_routes');
+const postRoutes = require('./post_routes');
+const commentRoutes = require('./comment_route');
 
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes)
 
 module.exports = router;
