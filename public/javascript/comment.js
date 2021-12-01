26067472f5758b30b39b60c913af60a8dd60ee09
jheckel <jalen.heckel@icloud.com>
2021-12-01 13:21:02 -0800

    added and updated comment.sj

---
+++ public/javascript/comment.js
@@ -0,0 +1,27 @@
+async function commentFormHandler(event) {
+    event.preventDefault();
+
+    const comment_text = document.querySelector('#comment-body').value.trim();
+
+    const post_id = document.querySelector('input[name="post-id"]').value;
+
+    if (comment_text) {
+        const response = await fetch ('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({
+                post_id,
+                comment_text
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        if (response.ok) {
+            document.location.reload();
+          } else {
+            alert(response.statusText);
+          }
+    }
+}
+
+document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
